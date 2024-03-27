629321816a9f5711b005c83590ecb518de394566
Kitkittipod <165009720+Kitkittipod@users.noreply.github.com>
2024-03-27 18:31:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const http = require('http');
 const mysql = require("mysql");
 const bodyParser = require('body-parser')
 const WebSocket = require('ws');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 app.use(cors())
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -681,6 +683,7 @@ app.get('/getIngredientsUsed', (req, res) => {
         }
     })
 })
+
 
 server.listen(3001, () => {
     console.log('Application is running on port 3001');
