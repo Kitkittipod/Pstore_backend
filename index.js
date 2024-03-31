43477b0da35ce92dc09e50ed9667e63d311ca91b
Kitkittipod <165009720+Kitkittipod@users.noreply.github.com>
2024-03-31 20:23:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const AWS = require('aws-sdk');
+
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const cors = require('cors');
@@ -33,23 +33,6 @@ wss.on('connection', (ws) => {
     })
 })
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-})
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: "public-read",
-        key: (req, file, cb) => {
-            cb(null, Date.now().toString() + '-' + file.originalname);
-        }
-    })
-})
 
 // db config
 const db = mysql.createConnection({
